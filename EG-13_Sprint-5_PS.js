ab3268a610a402758e8765336965af8b66b129d8
Asmual Obaidul Hoque <73558650+Asmual@users.noreply.github.com>
2026-09-18 21:28:25 +0600

    added invertTree solution with test case

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -89,3 +89,21 @@ var maxDepth = function(root) {
 // console.log(maxDepth(tree4));
 // Expected Output: 3
 
+/********** 05. Invert Binary Tree **********/
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if (root === null) return null;
+    let temp = root.left;
+    root.left = invertTree(root.right);
+    root.right = invertTree(temp);
+    return root;
+};
+
+// function TreeNode(val, left, right) { this.val = (val===undefined ? 0 : val); this.left = (left===undefined ? null : left); this.right = (right===undefined ? null : right); }
+// const tree5 = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(7, new TreeNode(6), new TreeNode(9)));
+// console.log(invertTree(tree5));
+// Expected Output: Inverted Tree
+
