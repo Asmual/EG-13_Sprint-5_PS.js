e1213fb62f32e6d1478a2b3cc124968eed292d51
Asmual Obaidul Hoque <73558650+Asmual@users.noreply.github.com>
2026-09-18 21:41:07 +0600

    added rotate solution with test case

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -134,3 +134,32 @@ var productExceptSelf = function(nums) {
 // console.log(productExceptSelf([1, 2, 3, 4]));
 // Expected Output: [24, 12, 8, 6]
 
+/********** 07. Rotate Array **********/
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    k = k % nums.length;
+
+    const reverse = (arr, start, end) => {
+        while (start < end) {
+            let temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    };
+
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+};
+
+// const nums7 = [1, 2, 3, 4, 5, 6, 7];
+// rotate(nums7, 3);
+// console.log(nums7);
+// Expected Output: [5, 6, 7, 1, 2, 3, 4]
+
