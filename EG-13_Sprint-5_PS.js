45b416c126a854167abf88d5c8ffc26234fc0d62
Asmual Obaidul Hoque <73558650+Asmual@users.noreply.github.com>
2026-09-18 21:41:37 +0600

    added checkSubarraySum solution with test case

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -215,3 +215,34 @@ MinStack.prototype.getMin = function() {
 // minStack.pop();
 // console.log(minStack.getMin()); // Expected Output: -2
 
+/********** 09. Continuous Subarray Sum **********/
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var checkSubarraySum = function(nums, k) {
+    const map = new Map();
+    map.set(0, -1);
+    let runningSum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        runningSum += nums[i];
+        let remainder = runningSum % k;
+        if (remainder < 0) remainder += k;
+
+        if (map.has(remainder)) {
+            if (i - map.get(remainder) >= 2) {
+                return true;
+            }
+        } else {
+            map.set(remainder, i);
+        }
+    }
+
+    return false;
+};
+
+// console.log(checkSubarraySum([23, 2, 4, 6, 7], 6));
+// Expected Output: true
+
